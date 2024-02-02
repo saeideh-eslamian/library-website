db423e0d71b79034b51ee65308d774bdf23ba6c5
dci-student <s.eslamian.70@gmail.cm>
2024-02-02 16:09:35 +0100

    add form search to template

--- library/urls.py
+++ library/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('search/', views.book_search, name='book_search'),
 ]
